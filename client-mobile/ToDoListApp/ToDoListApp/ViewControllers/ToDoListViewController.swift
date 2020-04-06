a7915aae9ee08c21d583f6c0eb21df89bc66ee32
corykim0829 <corykim0829@gmail.com>
2020-04-06 16:03:03 +0900

    [#3] Feat: ToDoListViewController UI

--- client-mobile/ToDoListApp/ToDoListApp/ViewControllers/ToDoListViewController.swift
+++ client-mobile/ToDoListApp/ToDoListApp/ViewControllers/ToDoListViewController.swift
@@ -12,9 +12,7 @@ class ToDoListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
 

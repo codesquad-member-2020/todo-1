6b15a8663ebdb77db56d7bbd990aae3b78da3ccd
corykim0829 <corykim0829@gmail.com>
2020-04-07 17:29:51 +0900

    [#5] Feat: 모든 TableView DataSource, Delegate 클래스 생성

--- client-mobile/ToDoListApp/ToDoListApp/Models/DoneCardListDataSource.swift
+++ client-mobile/ToDoListApp/ToDoListApp/Models/DoneCardListDataSource.swift
@@ -1,14 +1,14 @@
 //
-//  InProgressCardListDataSource.swift
+//  DoneCardListDataSource.swift
 //  ToDoListApp
 //
-//  Created by Cory Kim on 2020/04/06.
+//  Created by Cory Kim on 2020/04/07.
 //  Copyright © 2020 corykim0829. All rights reserved.
 //
 
 import UIKit
 
-class InProgressCardListDataSource: NSObject, UITableViewDataSource {
+class DoneCardListDataSource: NSObject, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 20
---
+++ client-mobile/ToDoListApp/ToDoListApp/Views/DoneCardListDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  DoneCardListDelegate.swift
+//  ToDoListApp
+//
+//  Created by Cory Kim on 2020/04/07.
+//  Copyright © 2020 corykim0829. All rights reserved.
+//
+
+import UIKit
+
+class DoneCardListDelegate: NSObject, UITableViewDelegate {
+}

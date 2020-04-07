ec2e6b6a671d7b4e0098939d4fb39b6476d82074
corykim0829 <corykim0829@gmail.com>
2020-04-07 22:43:58 +0900

    [#9] Feat: Column, Card 모델

---
+++ client-mobile/ToDoListApp/ToDoListApp/Models/Column.swift
@@ -0,0 +1,15 @@
+//
+//  Column.swift
+//  ToDoListApp
+//
+//  Created by Cory Kim on 2020/04/07.
+//  Copyright © 2020 corykim0829. All rights reserved.
+//
+
+import Foundation
+
+struct Column {
+    let identifier: Int
+    var name: String
+    var cards: [Card]
+}

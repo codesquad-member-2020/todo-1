1b240d3fe76006b011bcb3991bfd7065efa0d4b4
corykim0829 <corykim0829@gmail.com>
2020-04-07 22:49:17 +0900

    [#9] Refactor: 사용하지 않는 코드 삭제

--- client-mobile/ToDoListApp/ToDoListApp/ViewControllers/ToDoListViewController.swift
+++ client-mobile/ToDoListApp/ToDoListApp/ViewControllers/ToDoListViewController.swift
@@ -12,25 +12,16 @@ class ToDoListViewController: UIViewController {
 
     private let cardListViewControllerIdentifier = "CardList"
     
-    var todoTableView: UITableView!
-    var inProgressTableView: UITableView!
-    var doneTableView: UITableView!
-    
     @IBOutlet weak var stackView: UIStackView!
-    
-    let toDoCardListDataSource = ToDoCardListDataSource()
-    let inProgressCardListDataSource = InProgressCardListDataSource()
-    let doneCardListDataSource = DoneCardListDataSource()
-    
-    let toDoCardListDelegate = ToDoCardListDelegate()
-    let inProgressCardListDelegate = InProgressCardListDelegate()
-    let doneCardListDelegate = DoneCardListDelegate()
     
     private var columns: [Column] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configureCardList()
+    }
+    
+    private func configureCardList() {
         columns = [Column(identifier: 1, name: "해야할 일", cards: []),
                    Column(identifier: 2, name: "하고있는 일", cards: []),
                    Column(identifier: 3, name: "완료한 일", cards: [])]
@@ -40,23 +31,6 @@ class ToDoListViewController: UIViewController {
             self.addChild(cardList)
             self.stackView.addArrangedSubview(cardList.view)
         }
-    }
-    
-    private func configureCardList() {
-        configureCardListDatasource()
-        configureCardListDelegate()
-    }
-    
-    private func configureCardListDatasource() {
-        todoTableView.dataSource = toDoCardListDataSource
-        inProgressTableView.dataSource = inProgressCardListDataSource
-        doneTableView.dataSource = doneCardListDataSource
-    }
-    
-    private func configureCardListDelegate() {
-        todoTableView.delegate = toDoCardListDelegate
-        inProgressTableView.delegate = inProgressCardListDelegate
-        doneTableView.delegate = doneCardListDelegate
     }
 }
 

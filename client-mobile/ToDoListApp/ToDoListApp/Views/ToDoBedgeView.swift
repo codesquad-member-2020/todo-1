9838bdc50f729547b398951576cfb38c53ae9a2c
corykim0829 <corykim0829@gmail.com>
2020-04-07 17:18:29 +0900

    [#5] Feat: 커스텀 BadgeView 추가

---
+++ client-mobile/ToDoListApp/ToDoListApp/Views/ToDoBedgeView.swift
@@ -0,0 +1,29 @@
+//
+//  CardListHeaderView.swift
+//  ToDoListApp
+//
+//  Created by Cory Kim on 2020/04/07.
+//  Copyright © 2020 corykim0829. All rights reserved.
+//
+
+import UIKit
+
+class ToDoBedgeView: UIView {
+    
+    fileprivate let height: CGFloat = 26
+    var badgeLabel: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = height / 2
+    }
+}

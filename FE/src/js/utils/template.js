7f2ca20b9307bfa432f1985c6ffa722fe0c4bbe7
Reese Kim <reesekimm@gmail.com>
2020-04-08 21:11:33 +0900

    [#28] Header 렌더링 기능 추가

---
+++ FE/src/js/utils/template.js
@@ -0,0 +1,14 @@
+export function header() {
+	return `<header>
+		<div class="header-container">
+			<h1>Todo List</h1>
+			<div class="menu">
+				<i class="fas fa-bars"></i>
+			</div>
+		</div>
+	</header>`;
+}
+
+export function columnContainer() {
+	return ``;
+}

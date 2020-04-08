cc40a9bed7111a4a0942b17ff1d7a753b72fb6e2
corykim0829 <corykim0829@gmail.com>
2020-04-08 23:25:57 +0900

    [#26] Refactor: 사용안하는 코드 삭제

--- client-mobile/ToDoListApp/ToDoListApp/Views/CardList/CardCell.swift
+++ client-mobile/ToDoListApp/ToDoListApp/Views/CardList/CardCell.swift
@@ -33,11 +33,4 @@ class CardCell: UITableViewCell {
         super.awakeFromNib()
         
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }

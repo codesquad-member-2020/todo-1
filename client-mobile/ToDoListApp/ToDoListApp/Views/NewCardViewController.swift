f475f292d4210a8eb2d07e753634988b076e2b80
corykim0829 <corykim0829@gmail.com>
2020-04-08 13:41:14 +0900

    [#13] Feat: 스토리보드 UI 구현

---
+++ client-mobile/ToDoListApp/ToDoListApp/Views/NewCardViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NewCardViewController.swift
+//  ToDoListApp
+//
+//  Created by Cory Kim on 2020/04/08.
+//  Copyright © 2020 corykim0829. All rights reserved.
+//
+
+import UIKit
+
+class NewCardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}

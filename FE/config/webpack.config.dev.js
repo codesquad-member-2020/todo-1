811a8c6eddd161610098c94bc58f9127bbdb4310
Reese Kim <reesekimm@gmail.com>
2020-04-08 14:17:01 +0900

    Feat: webpack.config.dev 수정 및 테스트
    빌드 결과물
    - login.html / todo.html / login.js / todo.js / style.css
    
    아래와 같이 동작하도록 웹팩 설정 수정
    - login 화면 : login.html + login.js + style.css(공통)
    - todo 화면 : todo.html + todo.js + style.css(공통)

--- FE/config/webpack.config.dev.js
+++ FE/config/webpack.config.dev.js
@@ -1,12 +1,17 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const ENTRY_FILE = path.resolve(__dirname, "../src", "js", "main.js");
+const ENTRY_FILE_LOGIN = path.resolve(__dirname, "../src", "js", "login.js");
+const ENTRY_FILE_TODO = path.resolve(__dirname, "../src", "js", "todo.js");
 const OUTPUT_DIR = path.resolve(__dirname, "../src", "static");
 
 const config = {
 	mode: "development",
-	entry: ENTRY_FILE,
+	entry: {
+		login: ENTRY_FILE_LOGIN,
+		todo: ENTRY_FILE_TODO,
+	},
 	resolve: {
 		alias: {
 			Scss: path.resolve(__dirname, "../src/scss/"),
@@ -15,6 +20,18 @@ const config = {
 	plugins: [
 		new MiniCssExtractPlugin({
 			filename: "style.css",
+		}),
+		new HtmlWebpackPlugin({
+			filename: "login.html",
+			template: path.join(__dirname, "../src/login.html"),
+			hash: true,
+			chunks: ["login"],
+		}),
+		new HtmlWebpackPlugin({
+			filename: "todo.html",
+			template: path.join(__dirname, "../src/todo.html"),
+			hash: true,
+			excludeChunks: ["login"],
 		}),
 	],
 	module: {
@@ -33,7 +50,7 @@ const config = {
 	},
 	output: {
 		path: OUTPUT_DIR,
-		filename: "bundle.js",
+		filename: "[name].js",
 	},
 };
 

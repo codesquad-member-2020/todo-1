39ed10f59cebdaa9b88f7842eb06b3d4e3747e24
Reese Kim <reesekimm@gmail.com>
2020-04-08 19:34:54 +0900

    [#24] 로그인 프로세스에 서버 요청 로직 추가

--- FE/src/js/login.js
+++ FE/src/js/login.js
@@ -1,6 +1,17 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 import "Scss/styles.scss";
+import data from "./data";
 
 const replacePage = () => location.replace("todo.html");
-document.querySelector(".login").addEventListener("click", replacePage);
+
+const showAlert = () => alert("Oopsy! Something went wrong.");
+
+const handleLogin = () => {
+	// http.post
+	// 응답 받으면 데이터를 data.column에 할당
+	// todo.js가 import해서 사용
+	data.columns ? replacePage() : showAlert();
+};
+
+document.querySelector(".login").addEventListener("click", handleLogin);

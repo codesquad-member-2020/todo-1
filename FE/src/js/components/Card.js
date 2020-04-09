9ebf8e558b08fa10fbb8465c979bf9c0be90659b
Reese Kim <reesekimm@gmail.com>
2020-04-10 02:33:37 +0900

    [#32] Feat: Card 삭제 버튼에 이벤트 리스너 바인딩

--- FE/src/js/components/Card.js
+++ FE/src/js/components/Card.js
@@ -5,9 +5,19 @@ export default class Card {
 		this.$target = $target;
 		this.data = data;
 		this.render();
+		this.bindeEventListener();
 	}
 
 	render() {
 		this.$target.insertAdjacentHTML("afterbegin", card(this.data));
+	}
+
+	bindeEventListener() {
+		this.$target.addEventListener("click", (e) => this.deleteCard(e));
+	}
+
+	deleteCard(e) {
+		e.stopImmediatePropagation();
+		console.log(e.target);
 	}
 }

00762080f08e1736b8656f6aaf1ade8e9b714451
Reese Kim <reesekimm@gmail.com>
2020-04-10 04:34:10 +0900

    [#32] Feat: Alert창 취소 버튼 클릭시 창 닫힘 기능 구현

--- FE/src/js/components/Alert.js
+++ FE/src/js/components/Alert.js
@@ -2,17 +2,30 @@ import { alert } from "../utils/template";
 
 export default class Alert {
 	$alert = null;
+	$cancel = null;
+	$confirm = null;
 
 	constructor({ data, onConfirm }) {
 		this.data = data;
 		this.onConfirm = onConfirm;
 
 		this.render();
+		this.cacheDomElements();
+		this.bindEventListenr();
 	}
 
 	render() {
 		document.getElementById("Todo").insertAdjacentHTML("afterend", alert(this.data.message));
+	}
+
+	cacheDomElements() {
 		this.$alert = document.querySelector(".alert");
+		this.$cancel = this.$alert.querySelector(".cancel");
+		this.$confirm = this.$alert.querySelector(".confirm");
+	}
+
+	bindEventListenr() {
+		this.$cancel.addEventListener("click", this.toggleDisplay.bind(this, { visible: false }));
 	}
 
 	toggleDisplay(nextData) {

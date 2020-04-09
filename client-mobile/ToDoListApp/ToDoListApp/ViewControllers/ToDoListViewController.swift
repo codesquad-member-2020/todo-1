7aa936b92268c3f6e0218bec34479efae5b41b7b
corykim0829 <corykim0829@gmail.com>
2020-04-09 23:05:22 +0900

    [#38] Feat: 네트워크 에러 처리

--- client-mobile/ToDoListApp/ToDoListApp/ViewControllers/ToDoListViewController.swift
+++ client-mobile/ToDoListApp/ToDoListApp/ViewControllers/ToDoListViewController.swift
@@ -15,6 +15,10 @@ class ToDoListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureToDoList()
+    }
+    
+    private func configureToDoList() {
         requestColumnsData { (columns) in
             DispatchQueue.main.async {
                 self.configureColumns(columns)
@@ -25,12 +29,21 @@ class ToDoListViewController: UIViewController {
     private func requestColumnsData(completion: @escaping ([Column]) -> Void) {
         MockNetworkManager.shared.requestColumns { (columns, error) in
             if error != nil {
-                // alert
+                self.showErrorAlert()
                 return
             }
             guard let columns = columns else { return }
             completion(columns)
         }
+    }
+    
+    private func showErrorAlert() {
+        let alert = UIAlertController(title: "Network Error", message: "Failed to load data", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Done", style: .default) { _ in
+            self.configureToDoList()
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
     
     private func configureColumns(_ columns: [Column]) {

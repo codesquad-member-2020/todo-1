7252db42e6b8a71ca6a9990f36f8bdef4a3c6445
Sosah <thstkd92@gmail.com>
2020-04-10 16:06:05 +0900

    IP 주소 변경 및 비밀번호 변경

--- BE/src/main/resources/data.sql
+++ BE/src/main/resources/data.sql
@@ -2,7 +2,7 @@ insert into todo (column_name, section) values ('Todo', 0);
 insert into todo (column_name, section) values ('In Progress', 1);
 insert into todo (column_name, section) values ('Done', 2);
 
-insert into user (user_id, password) values ('sunny', '123');
+insert into user (user_id, password) values ('sunny', '1234');
 
 insert into card (user_id, title, contents, device, row, todo, todo_key) VALUES
 (1, '아니', '배고프다', 'web', 1, 1, 1);

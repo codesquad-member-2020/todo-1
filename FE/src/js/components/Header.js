892f921e9dea2e4f3dd4627f05c05fd77a142e26
Reese Kim <reesekimm@gmail.com>
2020-04-13 21:49:08 +0900

    [#61] Activity 애니메이션 구현 Resolve #61

--- FE/src/js/components/Header.js
+++ FE/src/js/components/Header.js
@@ -2,14 +2,30 @@ import { header } from "../utils/template";
 import Activity from "./Activity";
 
 export default class Header {
+	$menuButton = null;
+
 	constructor($target) {
 		this.$target = $target;
 		this.render();
+		this.cacheDomElements();
+		this.bindEventListener();
 
-		this.activity = new Activity($target);
+		this.activity = new Activity({ $target, data: { visible: false } });
 	}
 
 	render() {
 		this.$target.insertAdjacentHTML("afterbegin", header());
+	}
+
+	cacheDomElements() {
+		this.$menuButton = this.$target.querySelector(".menu");
+	}
+
+	bindEventListener() {
+		this.$menuButton.addEventListener("click", this.handleActivityDisplay.bind(this));
+	}
+
+	handleActivityDisplay() {
+		this.activity.toggleDisplay({ visible: true });
 	}
 }

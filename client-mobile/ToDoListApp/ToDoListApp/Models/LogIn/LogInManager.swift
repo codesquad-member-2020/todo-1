03712f039ed3191744cd7df588ac561ac19d65ca
corykim0829 <corykim0829@gmail.com>
2020-04-14 01:12:23 +0900

    [#72] Feat: LogInManager

---
+++ client-mobile/ToDoListApp/ToDoListApp/Models/LogIn/LogInManager.swift
@@ -0,0 +1,24 @@
+//
+//  LogInManager.swift
+//  ToDoListApp
+//
+//  Created by Cory Kim on 2020/04/14.
+//  Copyright © 2020 corykim0829. All rights reserved.
+//
+
+import Foundation
+
+class LogInManager {
+    
+    private let tokenKey = "JWTToken"
+    private var JWTToken: String? = nil
+    
+    func saveToken(_ token: String) {
+        UserDefaults.standard.set(JWTToken, forKey: tokenKey)
+    }
+    
+    func loadToken() -> String? {
+        let token = UserDefaults.standard.object(forKey: tokenKey) as? String
+        return token
+    }
+}

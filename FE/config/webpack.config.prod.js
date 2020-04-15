ec277c6699f0ad3ad29ae85c98ebe852b6aa254d
Reese Kim <reesekimm@gmail.com>
2020-04-15 20:30:50 +0900

    [#83] Refactor: html, js 분리
    html파일을 하나로 통합관리할 경우 todo list 화면에서 페이지 새로고침시 login화면으로 돌아가는 문제 발생
    
    index.html -> login.html, todo.html로 분리
    main.js -> login.js, todo.js로 분리

--- FE/config/webpack.config.prod.js
+++ FE/config/webpack.config.prod.js
@@ -2,12 +2,16 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const ENTRY_FILE = path.resolve(__dirname, "../src", "js", "main.js");
+const ENTRY_FILE_LOGIN = path.resolve(__dirname, "../src", "js", "login.js");
+const ENTRY_FILE_TODO = path.resolve(__dirname, "../src", "js", "todo.js");
 const OUTPUT_DIR = path.resolve(__dirname, "../build");
 
 const config = {
 	mode: "production",
-	entry: ENTRY_FILE,
+	entry: {
+		login: ENTRY_FILE_LOGIN,
+		todo: ENTRY_FILE_TODO,
+	},
 	resolve: {
 		alias: {
 			Scss: path.resolve(__dirname, "../src/scss/"),
@@ -40,8 +44,16 @@ const config = {
 			filename: "style.css",
 		}),
 		new HtmlWebpackPlugin({
-			template: path.join(__dirname, "../src/index.html"),
+			filename: "login.html",
+			template: path.join(__dirname, "../src/login.html"),
 			hash: true,
+			chunks: ["login"],
+		}),
+		new HtmlWebpackPlugin({
+			filename: "todo.html",
+			template: path.join(__dirname, "../src/todo.html"),
+			hash: true,
+			excludeChunks: ["login"],
 		}),
 	],
 	output: {

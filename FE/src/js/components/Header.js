f02345ea7b5c0f8c59960803b09798d083d40519
Reese Kim <reesekimm@gmail.com>
2020-04-15 21:42:05 +0900

    [#83] Feat: activity 서버 get 요청 기능 추가 Resolve #83

--- FE/src/js/components/Header.js
+++ FE/src/js/components/Header.js
@@ -1,8 +1,13 @@
 import { header } from "../utils/template";
 import Activity from "./Activity";
+import HttpRequestHandler from "../utils/HttpRequestHandler";
+import { BASE_URL } from "../utils/const";
+import { handleError } from "../utils/utilFunction";
+import activityData from "../activityData";
 
 export default class Header {
 	$menuButton = null;
+	http = new HttpRequestHandler();
 
 	constructor($target) {
 		this.$target = $target;
@@ -26,6 +31,15 @@ export default class Header {
 	}
 
 	handleActivityDisplay() {
-		this.activity.toggleDisplay({ visible: true });
+		this.http
+			.get(`${BASE_URL}/activity`)
+			.then((response) => {
+				if (response.status === 200) {
+					this.activity.openActivity(activityData);
+				} else {
+					throw Error("네트워크 에러가 발생했습니다. 새로고침후 다시 시도해주세요.");
+				}
+			})
+			.catch(handleError);
 	}
 }

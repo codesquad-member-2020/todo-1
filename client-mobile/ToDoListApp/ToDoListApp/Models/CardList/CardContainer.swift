fe33714afdb8f017f256d24c6075dbc1a26e7955
corykim0829 <corykim0829@gmail.com>
2020-04-15 18:16:23 +0900

    [#95] Feat: Card 객체 디코딩을 위한 CardContainer 모델 추가

---
+++ client-mobile/ToDoListApp/ToDoListApp/Models/CardList/CardContainer.swift
@@ -0,0 +1,14 @@
+//
+//  CardContainer.swift
+//  ToDoListApp
+//
+//  Created by Cory Kim on 2020/04/15.
+//  Copyright © 2020 corykim0829. All rights reserved.
+//
+
+import Foundation
+
+struct CardContainer: Decodable {
+    let status: Int
+    let card: Card
+}

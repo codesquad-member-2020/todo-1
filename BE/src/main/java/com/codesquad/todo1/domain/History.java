cf3b61b8dd898d61da03fcc5fce7d73048157451
Sosah <thstkd92@gmail.com>
2020-04-16 15:27:31 +0900

    [#108] MySQL 시간 설정
    데이터 저장할 때 UTC시간이 아닌 서울시각 기준으로 String 값으로 저장하도록 설정.

--- BE/src/main/java/com/codesquad/todo1/domain/History.java
+++ BE/src/main/java/com/codesquad/todo1/domain/History.java
@@ -6,7 +6,9 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
 
-import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @ToString
@@ -21,17 +23,18 @@ public class History {
     private String title;
     private Integer fromColumn;
     private Integer toColumn;
-    private LocalDateTime actionTime;
+    private String actionTime;
 
     @Builder
     public History(String userId, String profileUrl, String action, String title,
-                   Integer fromColumn, Integer toColumn, LocalDateTime actionTime) {
+                   Integer fromColumn, Integer toColumn, String actionTime) {
         this.userId = userId;
         this.profileUrl = profileUrl;
         this.action = action;
         this.title = title;
         this.fromColumn = fromColumn;
         this.toColumn = toColumn;
-        this.actionTime = actionTime;
+        this.actionTime = ZonedDateTime.now(ZoneId.of("Asia/Seoul"))
+                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
     }
 }

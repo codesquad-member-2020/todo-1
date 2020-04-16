7c5e137ff949ba5ff2cda6149d7bfd56a91108f4
Sosah <thstkd92@gmail.com>
2020-04-16 23:18:59 +0900

    [#116] Login의 내부로직 UserService로 이동 및 리팩토링
    - makeJwt(): jwt토큰을 만들어 쿠키에 담는 역할.
    
    - checkValidation(): User savedUser는 중복되는 코드라 추가 리팩토링이 가능.

--- BE/src/main/java/com/codesquad/todo1/controller/LoginController.java
+++ BE/src/main/java/com/codesquad/todo1/controller/LoginController.java
@@ -3,7 +3,6 @@ package com.codesquad.todo1.controller;
 import com.codesquad.todo1.api.ApiUserInfo;
 import com.codesquad.todo1.error.AuthorizationFail;
 import com.codesquad.todo1.service.UserService;
-import com.codesquad.todo1.utils.JwtUtils;
 import com.codesquad.todo1.api.ApiLogin;
 import com.codesquad.todo1.domain.User;
 import lombok.RequiredArgsConstructor;
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -22,26 +20,16 @@ import javax.servlet.http.HttpServletResponse;
 @RequiredArgsConstructor
 public class LoginController {
 
-    private Logger logger = LoggerFactory.getLogger(LoginController.class);
+    private final Logger logger = LoggerFactory.getLogger(LoginController.class);
     private final UserService userService;
 
     @PostMapping("login")
     public ApiLogin login(@RequestBody User user,
                         HttpServletResponse response) {
-        String userId = user.getUserId();
-        String password = user.getPassword();
-        //todo: 해당 복잡한 로직은 UserService에 넣도록 하기.
-        logger.info("loginTryUser : {}", user);
         try {
-            User validationSuccessUser = checkValidation(userId, password);
-            // 인증 통과한 userId로 jwt를 만든다.
-            String jwt = JwtUtils.jwtCreate(userId);
-            // 만든 jwt를 쿠키에 담아서 response 한다.
-            Cookie cookie = new Cookie("jwt", jwt);
-            response.addCookie(cookie);
-            response.setStatus(200);
+            String jwt = userService.makeJwt(user, response);
             return new ApiLogin(200, jwt);
-        } catch (Exception e) {
+        } catch (RuntimeException e) {
             response.setStatus(401);
             return new ApiLogin(401, null);
         }
@@ -53,15 +41,5 @@ public class LoginController {
         User user = userService.findByUserId(userInfo).orElseThrow(() ->
                 new IllegalStateException("No User"));
         return new ApiUserInfo(user);
-    }
-        //todo: UserService 로직에 포함 하기.
-    private User checkValidation(String userId, String password) throws AuthorizationFail {
-        User savedUser = userService.findByUserId(userId).orElseThrow(AuthorizationFail::new);
-        logger.info("savedUser : {}", savedUser);
-        String savedPassword = savedUser.getPassword();
-        if (!password.equals(savedPassword)) {
-            throw new AuthorizationFail();
-        }
-        return savedUser;
     }
 }
--- BE/src/main/java/com/codesquad/todo1/service/UserService.java
+++ BE/src/main/java/com/codesquad/todo1/service/UserService.java
@@ -1,11 +1,15 @@
 package com.codesquad.todo1.service;
 
 import com.codesquad.todo1.domain.User;
+import com.codesquad.todo1.error.AuthorizationFail;
 import com.codesquad.todo1.repository.UserRepository;
+import com.codesquad.todo1.utils.JwtUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Optional;
 
 @Service
@@ -17,5 +21,27 @@ public class UserService {
     @Transactional
     public Optional<User> findByUserId(String userId) {
         return userRepository.findByUserId(userId);
+    }
+
+    public String makeJwt(User user, HttpServletResponse response) {
+        String userId = user.getUserId();
+        String password = user.getPassword();
+        User validationSuccessUser = checkValidation(userId, password);
+        // 인증 통과한 userId로 jwt를 만든다.
+        String jwt = JwtUtils.jwtCreate(validationSuccessUser.getUserId());
+        // 만든 jwt를 쿠키에 담아서 response 한다.
+        Cookie cookie = new Cookie("jwt", jwt);
+        response.addCookie(cookie);
+        response.setStatus(200);
+        return jwt;
+    }
+
+    private User checkValidation(String userId, String password) {
+        User savedUser = userRepository.findByUserId(userId).orElseThrow(AuthorizationFail::new);
+        String savedPassword = savedUser.getPassword();
+        if (!password.equals(savedPassword)) {
+            throw new AuthorizationFail();
+        }
+        return savedUser;
     }
 }

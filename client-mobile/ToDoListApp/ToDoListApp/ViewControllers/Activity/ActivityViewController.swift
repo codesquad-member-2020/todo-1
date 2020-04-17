9e75a817bf3017a2bb86954a237d4620050b61dd
corykim0829 <corykim0829@gmail.com>
2020-04-17 14:33:09 +0900

    [#130] Feat: 네트워크 요청 후 테이블 뷰 리로드

--- client-mobile/ToDoListApp/ToDoListApp/ViewControllers/Activity/ActivityViewController.swift
+++ client-mobile/ToDoListApp/ToDoListApp/ViewControllers/Activity/ActivityViewController.swift
@@ -12,9 +12,46 @@ class ActivityViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
+    private var activities: [Activity] = [] {
+        didSet { self.tableView.reloadData() }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        fetchActivities()
+        configureTableView()
+    }
+    
+    private func fetchActivities() {
+        NetworkManager.shared.requestData(path: "/api/activity") { (result: Result<ActivityContainer, RequestError>) in
+            
+            switch result {
+            case .success(let activityContainer):
+                DispatchQueue.main.async {
+                    self.activities = activityContainer.activities
+                }
+            case .failure(_):
+                break
+            }
+        }
+    }
+    
+    private func configureTableView() {
+        tableView.dataSource = self
+    }
+}
+
+extension ActivityViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return activities.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ActivityCell.identifier, for: indexPath) as! ActivityCell
+        let activity = activities[indexPath.item]
+        cell.updateCell(with: activity)
+        return cell
     }
 }

3ee8ebefb0d2bc8a62e7be1530cf7422ae9992a5
corykim0829 <corykim0829@gmail.com>
2020-04-17 14:38:02 +0900

    [#130] Refactor: profileImageView 테두리 제거

--- client-mobile/ToDoListApp/ToDoListApp/Views/Activity/ActivityCell.swift
+++ client-mobile/ToDoListApp/ToDoListApp/Views/Activity/ActivityCell.swift
@@ -39,8 +39,6 @@ class ActivityCell: UITableViewCell {
     
     private func configureProfileImageView() {
         profileImageView.layer.cornerRadius = profileImageHeight / 2
-        profileImageView.layer.borderWidth = 1
-        profileImageView.layer.borderColor = UIColor.lightGray.cgColor
         profileImageView.clipsToBounds = true
     }
 }

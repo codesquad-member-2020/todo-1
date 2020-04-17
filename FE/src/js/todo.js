8b9e0ead22bc5367901bcef9dbfd07eff960492f
hyunjun <guswns1659@gmail.com>
2020-04-17 14:12:58 +0900

    리뷰위해서 FE, iOS 폴더 삭제

--- FE/src/js/todo.js
+++
@@ -1,9 +0,0 @@
-import "core-js/stable";
-import "regenerator-runtime/runtime";
-import "Scss/styles.scss";
-import data from "./data";
-import Todo from "./components/Todo";
-
-const $target = document.querySelector("#Todo");
-
-new Todo({ $target, data });

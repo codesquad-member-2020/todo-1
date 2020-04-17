798f9fb07622aa6e32f301860fc46eba5f91fa28
corykim0829 <corykim0829@gmail.com>
2020-04-17 15:17:29 +0900

    [#130] Feat: action에 따라 activity 문구 변경

--- client-mobile/ToDoListApp/ToDoListApp/Views/Activity/ActivityCell.swift
+++ client-mobile/ToDoListApp/ToDoListApp/Views/Activity/ActivityCell.swift
@@ -17,6 +17,7 @@ class ActivityCell: UITableViewCell {
     @IBOutlet weak var timeLabel: UILabel!
     
     private let profileImageHeight: CGFloat = 40
+    private let fontSize: CGFloat = 17
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -25,7 +26,63 @@ class ActivityCell: UITableViewCell {
     }
     
     func updateCell(with activity: Activity) {
-        NetworkManager.shared.requestData(from: activity.profileURL) { (result) in
+        fetchImage(with: activity.profileURL)
+        actionLabel.attributedText = updateActionLabel(with: activity)
+    }
+    
+    private func updateActionLabel(with activity: Activity) -> NSAttributedString {
+        let userId = activity.userId
+        let action = activity.action
+        let title = activity.title
+        
+        let attributedText = NSMutableAttributedString(string: "@\(userId) ", attributes:
+            [.font: UIFont.systemFont(ofSize: fontSize, weight: .bold),
+             .foregroundColor: #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)])
+        
+        var actionBehavior = ""
+        var firstProposition = ""
+        var secondProposition = ""
+        var fromColumn = ""
+        var toColumn = ""
+        
+        switch action {
+        case "add":
+            actionBehavior = "added "
+            firstProposition = "to "
+            toColumn = "\(activity.toColumn) "
+            break
+        case "removed":
+            actionBehavior = "removed "
+            secondProposition = "at "
+            toColumn = "\(activity.toColumn) "
+            break
+        case "move":
+            actionBehavior = "moved "
+            firstProposition = "from "
+            fromColumn = "\(activity.fromColumn) "
+            secondProposition = "to "
+            toColumn = "\(activity.toColumn) "
+            break
+        case "update":
+            actionBehavior = "edited as "
+            secondProposition = "at "
+            toColumn = "\(activity.toColumn) "
+            break
+        default:
+            break
+        }
+        
+        attributedText.append(NSAttributedString(string: actionBehavior, attributes: [.font: UIFont.systemFont(ofSize: fontSize, weight: .regular)]))
+        attributedText.append(NSAttributedString(string: "\(title) ", attributes: [.font: UIFont.systemFont(ofSize: fontSize, weight: .bold)]))
+        attributedText.append(NSAttributedString(string: firstProposition, attributes: [.font: UIFont.systemFont(ofSize: fontSize, weight: .regular)]))
+        attributedText.append(NSAttributedString(string: fromColumn, attributes: [.font: UIFont.systemFont(ofSize: fontSize, weight: .bold)]))
+        attributedText.append(NSAttributedString(string: secondProposition, attributes: [.font: UIFont.systemFont(ofSize: fontSize, weight: .regular)]))
+        attributedText.append(NSAttributedString(string: toColumn, attributes: [.font: UIFont.systemFont(ofSize: fontSize, weight: .bold)]))
+        return attributedText
+    }
+    
+    private func fetchImage(with profileURL: String) {
+        NetworkManager.shared.requestData(from: profileURL) { (result) in
             switch result {
             case .success(let data):
                 DispatchQueue.main.async {
@@ -40,5 +97,9 @@ class ActivityCell: UITableViewCell {
     private func configureProfileImageView() {
         profileImageView.layer.cornerRadius = profileImageHeight / 2
         profileImageView.clipsToBounds = true
+    }
+    
+    override func prepareForReuse() {
+        self.profileImageView.image = nil
     }
 }

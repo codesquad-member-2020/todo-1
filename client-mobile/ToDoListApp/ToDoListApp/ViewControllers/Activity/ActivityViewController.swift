b53ddbab0a87aa9cfaf159da1f32dde85f546c5a
corykim0829 <corykim0829@gmail.com>
2020-04-17 13:32:05 +0900

    [#130] Feat: Activity Cell 구현

---
+++ client-mobile/ToDoListApp/ToDoListApp/ViewControllers/Activity/ActivityViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ActivityViewController.swift
+//  ToDoListApp
+//
+//  Created by Cory Kim on 2020/04/17.
+//  Copyright © 2020 corykim0829. All rights reserved.
+//
+
+import UIKit
+
+class ActivityViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}

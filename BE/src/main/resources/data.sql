570904ca984731a08ecb376f60239a980224782a
Sunny92 <thstkd92@gmail.com>
2020-07-02 20:46:16 +0900

    fix: 유저 데이터 추가

--- BE/src/main/resources/data.sql
+++ BE/src/main/resources/data.sql
@@ -5,3 +5,4 @@ insert into category (column_name, project, project_key) values ('Done', 1, 3);
 
 insert into user (user_id, password, profile_url) values ('sunny', '1234', 'https://github.com/corykim0829/todo-1/blob/ios/feature/user-info-view/client-mobile/profileImages/sunny.png?raw=true');
 insert into user (user_id, password, profile_url) values ('cory', '12', 'https://github.com/corykim0829/todo-1/blob/ios/feature/user-info-view/client-mobile/profileImages/cory.png?raw=true');
+insert into user (user_id, password, profile_url) values ('reese', '1234', 'https://avatars0.githubusercontent.com/u/42695954?s=460&u=5227f8eb42e141c22cbffc2cc813e4d8ba2a9fd2&v=4');
